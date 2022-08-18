f7d70267c4286fe60d32a0908942698481248a19
zed-web-m <72280188+zed-web-m@users.noreply.github.com>
2022-08-18 21:34:23 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -73,3 +73,54 @@ const teamSlider = new Swiper('.team-slider', {
 // }
 //
 // animateMarquee(document.querySelector('.header-top'), 30000)
+
+function get_browser() {
+  var ua = navigator.userAgent, tem,
+      M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i) || [];
+  if (/trident/i.test(M[1])) {
+    tem = /\brv[ :]+(\d+)/g.exec(ua) || [];
+    return {name: 'IE', version: (tem[1] || '')};
+  }
+  if (M[1] === 'Chrome') {
+    tem = ua.match(/\bOPR|Edge\/(\d+)/)
+    if (tem != null) {
+      return {name: 'Opera', version: tem[1]};
+    }
+  }
+  M = M[2] ? [M[1], M[2]] : [navigator.appName, navigator.appVersion, '-?'];
+  if ((tem = ua.match(/version\/(\d+)/i)) != null) {
+    M.splice(1, 1, tem[1]);
+  }
+  return {
+    name: M[0],
+    version: M[1]
+  };
+}
+
+(function() {
+  scrollTo();
+})();
+
+function scrollTo() {
+  const links = document.querySelectorAll('.scroll');
+  links.forEach(each => (each.onclick = scrollAnchors));
+}
+
+function scrollAnchors(e, respond = null) {
+  const distanceToTop = el => Math.floor(el.getBoundingClientRect().top) - 80;
+  e.preventDefault();
+  var targetID = (respond) ? respond.getAttribute('href') : this.getAttribute('href');
+  const targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  const originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: 'smooth' });
+  const checkIfDone = setInterval(function() {
+    const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = '-1';
+      targetAnchor.focus();
+      window.history.pushState('', '', targetID);
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
